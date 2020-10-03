aa1a7cd36d8f7ae7892721e9944d25709e59e791
Vasilkevich Ilya <Vasilkevich@redsoft.ru>
2020-10-03 15:42:24 +0300

    feat: comlete task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
+exports.min = function min(array) {
+    if (!array || !array.length) return 0;
 
-exports.min = function min (array) {
-  return 0;
-}
+    let result = array[0];
 
-exports.max = function max (array) {
-  return 0;
-}
+    array.forEach((element) => {
+        result = element < result ? element : result;
+    });
+    return result;
+};
 
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (!array || !array.length) return 0;
+
+    let result = array[0];
+
+    array.forEach((element) => {
+        result = element > result ? element : result;
+    });
+    return result;
+};
+
+exports.avg = function avg(array) {
+    if (!array || !array.length) return 0;
+    const sum = array.reduce((acc, item) => {
+        return acc + item;
+    }, 0);
+
+
+    return sum/array.length;
+};
